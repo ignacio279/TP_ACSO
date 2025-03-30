54589dfb8c2c7e1065ba9080c7249f968097e3f1
ignacio <ivargasfernandez@udesa.edu.ar>
2025-03-30 19:26:15 -0300

    1

--- TP1-ARM/src/sim.c
+++ TP1-ARM/src/sim.c
@@ -23,7 +23,7 @@
 #define STURB        0b00111000000 //
 #define STURH        0b01111000000
 #define LDURB        0b00111000010
-#define LDUR         0b10111000010
+#define LDUR         0b11111000010
 #define ADD_IMM      0b10010001 
 #define ADD_EXT_REG  0b10001011001 
 #define CBZ          0b10110100
