04ffb1732ed55e7a0c930a87804226736b52b692
ignacio <ivargasfernandez@udesa.edu.ar>
2025-04-15 20:25:27 -0300

    j

--- TP2-x86_64/ej1/ej1.c
+++ TP2-x86_64/ej1/ej1.c
@@ -1,7 +1,15 @@
 #include "ej1.h"
 
-string_proc_list* string_proc_list_create(void){
+string_proc_list* string_proc_list_create(void) {
+    string_proc_list* list = (string_proc_list*)malloc(sizeof(string_proc_list));
+    if (list == NULL) {
+        return NULL;
+    }
+    list->first = NULL;
+    list->last  = NULL;
+    return list;
 }
+
 
 string_proc_node* string_proc_node_create(uint8_t type, char* hash){
 }
