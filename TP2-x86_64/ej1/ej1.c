a689cd98de95758d7e065b89d7de2d04674d9657
ignacio <ivargasfernandez@udesa.edu.ar>
2025-04-15 21:12:27 -0300

    dfads

--- TP2-x86_64/ej1/ej1.c
+++ TP2-x86_64/ej1/ej1.c
@@ -18,7 +18,6 @@ char* string_proc_list_concat(string_proc_list* list, uint8_t type , char* hash)
 void string_proc_list_destroy(string_proc_list* list){
 
 	/* borro los nodos: */
-	if (list == NULL) return;
 	string_proc_node* current_node	= list->first;
 	string_proc_node* next_node		= NULL;
 	while(current_node != NULL){
