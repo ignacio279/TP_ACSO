d96967290be0b7f74fe0125edfb7ddcc922854ba
ignacio <ivargasfernandez@udesa.edu.ar>
2025-04-15 21:48:28 -0300

    f

--- TP2-x86_64/ej1/ej1.c
+++ TP2-x86_64/ej1/ej1.c
@@ -11,15 +11,6 @@ string_proc_list* string_proc_list_create(void) {
 }
 
 string_proc_node* string_proc_node_create(uint8_t type, char* hash){
-	string_proc_node* node = (string_proc_node*)malloc(sizeof(string_proc_node));
-	if (node == NULL) {
-		return NULL;
-	}
-	node->type      = type;
-	node->hash      = hash;
-	node->next      = NULL;
-	node->previous  = NULL;
-	return node;
 }
 
 void string_proc_list_add_node(string_proc_list* list, uint8_t type, char* hash) {
