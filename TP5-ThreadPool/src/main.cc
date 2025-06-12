8fcbf689c4212b12ddc8c04b90b9183e1487f8fb
ignacio <ivargasfernandez@udesa.edu.ar>
2025-06-12 15:10:13 -0300

    j

--- TP5-ThreadPool/src/main.cc
+++ TP5-ThreadPool/src/main.cc
@@ -4,24 +4,19 @@
 #include "thread-pool.h"
 
 int main() {
-    // Crear un pool con 4 trabajadores
     ThreadPool pool(4);
 
     std::atomic<int> counter(0);
 
-    // Programar 100 tareas que incrementan el contador
     for (int i = 0; i < 100; ++i) {
         pool.schedule([&]() {
-            // Incrementar el contador
             counter++;
-            std::this_thread::sleep_for(std::chrono::milliseconds(10)); // Simula trabajo
+            std::this_thread::sleep_for(std::chrono::milliseconds(10)); 
         });
     }
 
-    // Esperar a que todas las tareas se completen
     pool.wait();
 
-    // Verificar que todas las tareas fueron ejecutadas
     std::cout << "Contador final = " << counter << std::endl;
 
     return 0;
